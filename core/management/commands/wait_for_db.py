20ccdc6bf633487ecc43daa90b54cc752bf0e1be
Jami Dwyer <jamidwyer@yahoo.com>
2024-06-11 13:39:48 -0700

    Add asterisk

--- core/management/commands/wait_for_db.py
+++ core/management/commands/wait_for_db.py
@@ -2,13 +2,14 @@ import time
 
 from psycopg2 import OperationalError as Psycopg2Error
 
+from django.db import connections
 from django.db.utils import OperationalError
 from django.core.management.base import BaseCommand
 
 
 class Command(BaseCommand):
 
-    def handle(self, args, **options):
+    def handle(self, *args, **options):
         self.stdout.write('Waiting for database...')
         db_up = False
         while db_up is False:
