39a9d4bbe328eaeb759aba2f846b37c8efc16f43
Jami Dwyer <jamidwyer@yahoo.com>
2024-07-23 17:03:28 -0700

    Fix underscore

--- inventory/schema.py
+++ inventory/schema.py
@@ -2,6 +2,7 @@ import graphene
 from graphene_django import DjangoObjectType
 from django_filters import OrderingFilter
 from .models import Product, InventoryItem
+from core.models import User
 
 
 class ProductType(DjangoObjectType):
@@ -14,21 +15,28 @@ class InventoryItemType(DjangoObjectType):
         model = InventoryItem
 
 
+class UserType(DjangoObjectType):
+    class Meta:
+        model = User
+
+
 class Query(graphene.ObjectType):
     products = graphene.List(ProductType)
 
-    inventoryItems = graphene.List(InventoryItemType)
+    inventory_items = graphene.List(InventoryItemType)
 
     order_by = OrderingFilter(
                fields=(('expiration_date'),))
 
-    class Arguments:
-        user_id = graphene.ID()
+    viewer = graphene.Field(lambda: Query)
+
+    def resolve_viewer(self, info):
+        return self
 
     def resolve_products(self, info, **kwargs):
         return Product.objects.all()
 
-    def resolve_inventory_items(self, info, user_id, **kwargs):
+    def resolve_inventory_items(self, info, **kwargs):
         return InventoryItem.objects.all()
 
 
