452494859afe196aa47219a73d31976af3d7cfd4
Jami Dwyer <jamidwyer@yahoo.com>
2024-08-13 08:54:29 -0700

    Add pagination

--- cms/settings.py
+++ cms/settings.py
@@ -155,6 +155,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 10
 }
 
 AUTH_USER_MODEL = 'core.User'
