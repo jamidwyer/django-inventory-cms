9c063758252742d0e1c20e9248675d08416ec2f7
Jami <jamidwyer@yahoo.com>
2024-10-22 16:07:21 -0700

    Update header

--- cms/settings.py
+++ cms/settings.py
@@ -15,8 +15,9 @@ from pathlib import Path
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent
 
+APPEND_SLASH = False
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
@@ -91,7 +92,7 @@ AUTHENTICATION_BACKENDS = [
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, "templates"), ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- cms/urls.py
+++ cms/urls.py
@@ -8,14 +8,15 @@ from django.urls import include, path
 from rest_framework import routers
 from cms.schema import schema
 from django.views.decorators.csrf import csrf_exempt
-
+from graphql_jwt.decorators import jwt_cookie
 
 router = routers.DefaultRouter()
 
 
 urlpatterns = [
-    path('graphql/', csrf_exempt(GraphQLView.as_view(graphiql=True,
-                                                     schema=schema))),
+    # TODO: csrf non-exempt
+    path('graphql', csrf_exempt(jwt_cookie(GraphQLView.as_view(graphiql=True,
+                                                     schema=schema)))),
     path('admin/', admin.site.urls),
     path('api/schema/', SpectacularAPIView.as_view(), name='api-schema'),
     path('api/docs/', SpectacularSwaggerView.as_view(url_name='api-schema'),
@@ -24,3 +25,7 @@ urlpatterns = [
     path('api/recipes/', include('recipes.urls')),
     path('api/inventory/', include('inventory.urls')),
 ]
+
+admin.site.site_header = 'Hord CMS'
+admin.site.site_title = 'Hord CMS'
+admin.site.index_title = 'Hord CMS'
